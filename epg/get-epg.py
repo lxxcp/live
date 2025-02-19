9e2ec1df4af62e8ea673aa4df3a3ff1a1eaaa946
lxxcp <lxxcp@163.com>
2025-02-19 13:14:28 +0800

    Update get-epg.py

--- epg/get-epg.py
+++ epg/get-epg.py
@@ -1,3 +1,5 @@
+@@ -1,96 +1,99 @@
+# -*- coding: utf-8 -*- 
 import re 
 import pytz 
 import requests 
@@ -6,21 +8,23 @@ from lxml import html
 from datetime import datetime, timezone, timedelta 
 
 tz = pytz.timezone('Asia/Shanghai')   
- 
+
 cctv_channel = ['cctv1', 'cctv2', 'cctv3', 'cctv4', 'cctv5', 'cctv5plus', 'cctv6', 
-                'cctv7', 'cctv8', 'cctvjilu', 'cctv10', 'cctv11', 'cctv12', 'cctv13', 'cctvchild', 
-                'cctv15', 'cctv16', 'cctv17', 'cctv4k'] 
+                'cctv7', 'cctv8', 'cctvjilu', 'cctv10', 'cctv11', 'cctv12', 'cctvchild', 
+                'cctv15', 'cctv16', 'cctv17', 'cctveurope','cctvamerica','cctv4k'] 
 cctv_channel_tvsou = ['cctv-1', 'cctv-2', 'cctv-3', 'cctv-4', 'cctv-5', 'cctv5+', 'cctv-6', 
                       'cctv-7', 'cctv-8', 'cctv-9', 'cctv-10', 'cctv-11', 'cctv-12'] 
- 
+
 sat_channel = ['cetv1', 'cetv2', 'cetv3', 'cetv4', 'btv1', 'btvjishi', 'dongfang', 
                'hunan', 'shandong', 'zhejiang', 'jiangsu', 'guangdong', 'dongnan', 'anhui', 
                'gansu', 'liaoning', 'travel', 'neimenggu', 'ningxia', 'qinghai', 'xiamen', 
+               'yunnan', 'chongqing', 'jiangxi', 'shan1xi', 'shan3xi', 'shenzhen', 'sichuan', 'tianjin', 
+               'guangxi', 'guizhou', 'hebei', 'henan', 'heilongjiang', 'hubei', 'jilin', 
                'yanbian', 'xizang', 'xinjiang', 'bingtuan', 'btvchild', 'gaoerfu', 'sdetv'] 
 sat_channel_tvsou = ['hubei', 'hunan', 'zhejiang', 'jiangsu', 'dongfang', 'btv1', 'guangdong', 
                       'shenzhen', 'heilongjiang', 'tianjin', 'shandong', 'anhui', 'liaoning'] 
- 
- 
+
+
 def get_epg_data(session, cids, epgdate): 
     try: 
         api = f"http://api.cntv.cn/epg/epginfo?c={cids}&d={epgdate}"   
@@ -31,8 +35,8 @@ def get_epg_data(session, cids, epgdate):
     except ValueError as e: 
         print(f"JSON decoding error: {e}") 
         return {} 
- 
- 
+
+
 def getChannelCNTV(fhandle, channelID): 
     ''' 
     通过央视cntv接口，获取央视，和上星卫视的节目单，写入同目录下 guide.xml   文件，文件格式符合xmltv标准 
@@ -50,24 +54,24 @@ def getChannelCNTV(fhandle, channelID):
     epgdate = datetime.now(tz).strftime('%Y%m%d')   
     session = requests.Session() 
     epgdata = get_epg_data(session, cids, epgdate) 
- 
+
     for channel in channelID: 
         if channel in epgdata: 
             # write channel id info 
             fhandle.write(f'      <channel id="{channel}">\n') 
             fhandle.write(f'          <display-name lang="cn">{epgdata[channel]["channelName"]}</display-name>\n') 
             fhandle.write('      </channel>\n') 
- 
- 
+
+
 def getChannelEPG(fhandle, channelID): 
     cids = ','.join(channelID) 
     session = requests.Session() 
     today = datetime.now(tz)   
     dates = [today + timedelta(days=i) for i in range(4)] 
     epgdates = [date.strftime('%Y%m%d') for date in dates] 
- 
+
     all_epg_data = [get_epg_data(session, cids, epgdate) for epgdate in epgdates] 
- 
+
     for channel in channelID: 
         for epgdata_current in all_epg_data: 
             if channel in epgdata_current: 
@@ -80,8 +84,8 @@ def getChannelEPG(fhandle, channelID):
                     fhandle.write(f'       <programme  channel="{channel}" start="{st}" stop="{et}" >\n') 
                     fhandle.write(f'           <title lang="zh">{detail["t"]}</title>\n') 
                     fhandle.write('       </programme>\n') 
- 
- 
+
+
 # 使用 gzip 打开文件进行压缩写入 
 with gzip.open('guide.xml.gz',  'wt', encoding='utf-8') as fhandle: 
     fhandle.write('<?xml   version="1.0" encoding="utf-8"?>\n') 
